a98dedf2b9b21825570503d427cd9e9f17a0bc44
SrihariSriyaPalaparthi <147176332+SrihariSriyaPalaparthi@users.noreply.github.com>
2025-04-22 20:27:06 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_hKl62CDIc', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'ap-south-1_9kZtM7SLI', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '56cjfqj5j0l4cf3klla0vt3tt0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
