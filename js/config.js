ebcb7cefbab140350eee2fa408b39a1827c965a2
SrihariSriyaPalaparthi <147176332+SrihariSriyaPalaparthi@users.noreply.github.com>
2025-04-22 19:27:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_hKl62CDIc
+', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '56cjfqj5j0l4cf3klla0vt3tt0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
